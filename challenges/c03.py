82a0729b008396c0d171ed05e662c87a3dd0aab4
redo <staehli.reto@gmail.com>
2022-06-28 23:04:46 +0200

    Adding solution for challenge 3

--- challenges/c03.py
+++ challenges/c03.py
@@ -11,18 +11,26 @@ def xor_all_keybytes(cipher: bytes) -> list[bytes]:
     return [xor_single_keybyte(key,cipher) for key in range(256)]
 
 def find_probable_cleartext(data: list[bytes]) -> bytes:
+    best_score = float("inf")
     for potential_cleartext in data:
-        freq_score(potential_cleartext)
-    return 
+        potential_cleartext_score = freq_score(potential_cleartext)
+        if potential_cleartext_score < best_score:
+            best_score = potential_cleartext_score
+            most_probable = potential_cleartext
+    return most_probable
 
-def freq_score(data: bytes) -> float:
+def freq_score(potential_cleartext: bytes) -> float:
     freqs = freq()
-    score = float("inf")
-    print("Enter loop")
-    for char in data:
+    potential_cleartext_str = ""
+    score = 0.0
+    for char in potential_cleartext:
         if 32 > char or char > 128:  # Decimal 0-31 (0x00-1F) and 127 (0x7F) are non-printable characters.
-            print(f"Breaking... Char is {char}")
             return float("inf")
+        potential_cleartext_str += chr(char)
+    for letter, frequency in freqs.items():
+        count = potential_cleartext_str.count(letter)
+        diff = frequency - count / len(potential_cleartext_str)
+        score += diff   
     return score
 
 def main() -> None:
@@ -31,6 +39,7 @@ def main() -> None:
     cipher_bytes = bytes.fromhex(hex_str)
     possible_cleartexts = xor_all_keybytes(cipher_bytes)
     probable_cleartext = find_probable_cleartext(possible_cleartexts)
+    print(f"Most probable cleartext is: {probable_cleartext.decode()}")
 
 if __name__ == "__main__":
     main()
