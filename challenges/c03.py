816aca60f06689df239cd88ca50d1007a410d4e5
redo <staehli.reto@gmail.com>
2022-06-28 21:22:27 +0200

    Working on challenge 3

--- challenges/c03.py
+++ challenges/c03.py
@@ -1,7 +1,36 @@
 #!/usr/bin/env python3
 
+from h01 import freq
+from c02 import fixed_xor
+from collections import Counter
+
+def xor_single_keybyte(key: int, cipher: bytes) -> bytes:
+    return fixed_xor(bytes([key] * len(cipher)), cipher)
+
+def xor_all_keybytes(cipher: bytes) -> list[bytes]:
+    return [xor_single_keybyte(key,cipher) for key in range(256)]
+
+def find_probable_cleartext(data: list[bytes]) -> bytes:
+    for potential_cleartext in data:
+        freq_score(potential_cleartext)
+    return 
+
+def freq_score(data: bytes) -> float:
+    freqs = freq()
+    score = float("inf")
+    print("Enter loop")
+    for char in data:
+        if 32 > char or char > 128:  # Decimal 0-31 (0x00-1F) and 127 (0x7F) are non-printable characters.
+            print(f"Breaking... Char is {char}")
+            return float("inf")
+    return score
+
 def main() -> None:
-    return
+    with open("../data/d03.txt", "r") as data:
+        hex_str = data.read()
+    cipher_bytes = bytes.fromhex(hex_str)
+    possible_cleartexts = xor_all_keybytes(cipher_bytes)
+    probable_cleartext = find_probable_cleartext(possible_cleartexts)
 
 if __name__ == "__main__":
     main()
---
+++ challenges/h01.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from collections import Counter
+import string
+from pprint import pprint
+
+def freq() -> dict[str, float]:
+    with open("../data/book.txt", "r") as f:
+        book = f.read()
+    book_lowercase_alphanumeric = "".join(filter(lambda x: x in string.ascii_lowercase, book))
+    char_count = Counter(book_lowercase_alphanumeric)
+    freqs = {letter: char_count[letter]/len(book_lowercase_alphanumeric) for letter in char_count}
+    return freqs
+
+def main() -> None:
+    char_freqs = freq()
+    pprint(char_freqs)
+    print(f"Most used character is: {max(char_freqs, key=char_freqs.get)}")
+    print(f"Top 5 used characters are {sorted(char_freqs, key=char_freqs.get, reverse=True)[:5]}")
+
+if __name__ == "__main__":
+    main()
