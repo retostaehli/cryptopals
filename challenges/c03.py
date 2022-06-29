55bf7c7c44cef0b29b66a84c0d0ecbb17fcb24a7
redo <staehli.reto@gmail.com>
2022-06-29 23:02:23 +0200

    Check ASCII range

--- challenges/c03.py
+++ challenges/c03.py
@@ -9,11 +9,12 @@ freqs = freq()
 def xor_single_keybyte(key: int, cipher: bytes) -> bytes:
     return fixed_xor(bytes([key] * len(cipher)), cipher)
 
-def xor_all_keybytes(cipher: bytes) -> list[bytes]:
-    return [xor_single_keybyte(key,cipher) for key in range(256)]
+def xor_all_keybytes(cipher_text: bytes) -> list[bytes]:
+    return [xor_single_keybyte(key,cipher_text) for key in range(256)]
 
 def find_probable_cleartext(data: list[bytes]) -> bytes:
     best_score = float("inf")
+    most_probable = data[0]
     for potential_cleartext in data:
         potential_cleartext_score = freq_score(potential_cleartext)
         if potential_cleartext_score < best_score:
@@ -25,7 +26,7 @@ def freq_score(potential_cleartext: bytes) -> float:
     potential_cleartext_str = ""
     score = 0.0
     for char in potential_cleartext:
-        if 32 > char or char > 128:  # Decimal 0-31 (0x00-1F) and 127 (0x7F) are non-printable characters.
+        if 1 > char < 127 :  # Decimal 0 - 8 and 127 are non-printable characters.
             return float("inf")
         potential_cleartext_str += chr(char)
     for letter, frequency in freqs.items():
@@ -34,16 +35,16 @@ def freq_score(potential_cleartext: bytes) -> float:
         score += diff   
     return score
 
-def break_single_byte_xor(hex_str: str) -> bytes:
-    cipher_bytes = bytes.fromhex(hex_str)
-    possible_cleartexts = xor_all_keybytes(cipher_bytes)
+def break_single_byte_xor(cipher_text: bytes) -> bytes:
+    possible_cleartexts = xor_all_keybytes(cipher_text)
     probable_cleartext = find_probable_cleartext(possible_cleartexts)
     return probable_cleartext
 
 def main() -> None:
     with open("../data/d03.txt", "r") as data:
         hex_str = data.read()
-    print(f"Most probable cleartext is: {break_single_byte_xor(hex_str).decode()}")
+    cipher_text = bytes.fromhex(hex_str)
+    print(f"Most probable cleartext is: {break_single_byte_xor(cipher_text).decode()}")
 
 if __name__ == "__main__":
     main()
