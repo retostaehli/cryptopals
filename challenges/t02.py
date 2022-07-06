ed01251a7d286f6300439fef65327f2ad2081ff9
redo <staehli.reto@gmail.com>
2022-07-06 22:48:18 +0200

    Fix indent

--- challenges/t02.py
+++ challenges/t02.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
 import unittest
-import c01
+import c02
 
-class T01(unittest.TestCase):
+class T02(unittest.TestCase):
 
-    def test_hex_to_b64(self):
-        self.assertEqual(c01.hex_to_b64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"),"SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t")
+    def test_fixed_xor(self):
+        self.assertEqual(c02.fixed_xor(bytes.fromhex("1c0111001f010100061a024b53535009181c"), bytes.fromhex("686974207468652062756c6c277320657965")).decode("utf-8"), "the kid don't play")
 
 if __name__ == "__main__":
     unittest.main()
