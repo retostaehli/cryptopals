72b3d602a1872b302f01c5e6dbc8cd52d536413a
redo <staehli.reto@gmail.com>
2022-07-06 22:48:34 +0200

    Fix indent

--- challenges/t05.py
+++ challenges/t05.py
@@ -6,7 +6,7 @@ import c05
 class T05(unittest.TestCase):
 
     def test_find_single_byte_xor(self):
-            self.assertEqual(c05.repeating_key_xor(bytes("ICE".encode()), bytes("Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal".encode())),bytes.fromhex("0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"))
+        self.assertEqual(c05.repeating_key_xor(bytes("ICE".encode()), bytes("Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal".encode())),bytes.fromhex("0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"))
 
 if __name__ == "__main__":
     unittest.main()
