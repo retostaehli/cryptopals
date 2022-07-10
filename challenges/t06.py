9bd57ddd8e7e5853edc3da5ff5e1ac3607ed3cef
redo <staehli.reto@gmail.com>
2022-07-10 11:55:53 +0200

    Test for challenge 06

--- challenges/t06.py
+++ challenges/t06.py
@@ -14,5 +14,6 @@ class T06(unittest.TestCase):
         with open("../data/d06.txt") as data:
             ciphertext = base64.b64decode(data.read())
         self.assertEqual(c06.break_repeating_key_xor(ciphertext)[0].decode().split("\n")[0], "I'm back and I'm ringin' the bell ")
+        
 if __name__ == "__main__":
     unittest.main()
