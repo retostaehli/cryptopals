a80fda9b5488eab90649effe01d8e8b7f23c53f7
redo <staehli.reto@gmail.com>
2022-09-06 19:07:25 +0200

    Add solution for challenge 9

---
+++ challenges/c09.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+def pad_pkcs7(plaintext: str, blocksize: int) -> bytes:
+    plaintext_bytes = bytes(plaintext, 'utf-8')
+    padlength = blocksize % len(plaintext_bytes)
+    if padlength: #We pad to a multiple of blocksize if plaintext is not a multiple of blocksize
+        return plaintext_bytes + padlength * padlength.to_bytes(1,'little')
+    return plaintext_bytes + blocksize * blocksize.to_bytes(1,'little') #We add a padding of lenght blocksize
+
+def unpad_pkcs7(plaintext: bytes) -> bytes:
+    padlength = plaintext[-1]
+    return plaintext[0:len(plaintext) - padlength]
+
+def main() -> None:
+    plaintext = "YELLOW SUBMARINE"
+    blocksize = 20
+    print(f"Padded: {pad_pkcs7(plaintext, blocksize)} - Unpadded: {unpad_pkcs7(pad_pkcs7(plaintext, blocksize))}")
+
+if __name__ == "__main__":
+    main()
