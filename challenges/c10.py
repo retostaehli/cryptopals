f4e68803ee8aedb8165067ead03df089459e5ac8
Reto Stähli <staehli.reto@gmail.com>
2023-09-11 15:54:33 +0200

    Work on CBC implementation

---
+++ challenges/c10.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+import base64
+from c02 import fixed_xor
+from c09 import pad_pkcs7, unpad_pkcs7
+from Crypto.Cipher import AES
+
+def encrypt_aes_cbc(cleartext: bytes, iv: bytes, key: bytes) -> bytes:
+    cipher = AES.new(key, AES.MODE_ECB)
+    plainblocks = [cleartext[i:i+len(key)] for i in range(0, len(cleartext), len(key))]
+    print(f"Length of cleartext bytes is {len(cleartext)}")
+    iv = iv
+    ciphertext = b""
+    for plainblock in plainblocks:
+        xorblock = fixed_xor(plainblock,iv)
+        aesblock = cipher.encrypt(xorblock)
+        ciphertext += aesblock
+        iv = aesblock
+    return ciphertext
+
+def decrypt_aes_cbc(ciphertext: bytes, iv: bytes, key: bytes) -> bytes:
+    cipher = AES.new(key, AES.MODE_ECB)
+    cipherblocks = [ciphertext[i:i+len(key)] for i in range(0, len(ciphertext), len(key))]
+    iv = iv
+    plaintext = b""
+    for cipherblock in cipherblocks:
+        xorblock = cipher.decrypt(cipherblock)
+        plainblock = fixed_xor(xorblock,iv)
+        iv = cipherblock
+        plaintext += plainblock
+
+    return plaintext
+
+def main() -> None:
+    key = b"YELLOW SUBMARINE"
+    with open("../data/d10.txt", "r") as data:
+        ciphertext = base64.b64decode(data.read())
+    print(f"Plaintext is {unpad_pkcs7(decrypt_aes_cbc(ciphertext,bytes(len(key)),key)).decode()}")
+
+if __name__ == "__main__":
+    main()
---
+++ challenges/t10.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import unittest
+import c10
+from c09 import pad_pkcs7, unpad_pkcs7
+
+class T10(unittest.TestCase):
+
+    def test_aes_cbc(self):
+        key = b"YELLOW SUBMARINE"
+        self.assertEqual(unpad_pkcs7(c10.decrypt_aes_cbc(c10.encrypt_aes_cbc(pad_pkcs7("This is a random string to encrypt",32),bytes(len(key)),key),bytes(len(key)),key)),bytes("This is a random string to encrypt","utf8"))
+
+if __name__ == "__main__":
+    unittest.main()
